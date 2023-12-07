507d37c2467dcfd7b9334ab8bc1c2e45cd6b4ec5
Gabriel Camargo <65323574+gabrielmadeiracamargo@users.noreply.github.com>
2023-12-07 16:05:27 -0300

    audio

---
+++ Gamejam Solsticio/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public AudioSource sfxsrc; // tem um audiosource pra musica e outro pra efeitos sonoros 
+    public AudioClip clock, jump, descend, death;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        //DontDestroyOnLoad(gameObject);
+    }
+}
--- Gamejam Solsticio/Assets/Scripts/OndaMovement.cs
+++ Gamejam Solsticio/Assets/Scripts/OndaMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,7 +9,7 @@ public class OndaMovement : MonoBehaviour
 {
     public int ondaAtual = 1;
     public Transform[] pontos;
-    public float timer; [SerializeField] TextMeshProUGUI text;
+    public float clocktimer, timer; [SerializeField] TextMeshProUGUI text;
 
     [SerializeField] Material material;
 
@@ -30,11 +31,19 @@ public class OndaMovement : MonoBehaviour
         death();
         
         timer += Time.deltaTime;
+        clocktimer += Time.deltaTime;
+
+        if (clocktimer >= 1f)
+        {
+            AudioManager.Instance.sfxsrc.PlayOneShot(AudioManager.Instance.clock);
+            clocktimer = 0;
+        }
     }
     void movement()
     {
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && ondaAtual < (pontos.Length - 1))
         {
+            AudioManager.Instance.sfxsrc.PlayOneShot(AudioManager.Instance.jump);
             ondaAtual += 1;
             Vector2 newPosition = new Vector2(this.gameObject.transform.position.x, pontos[ondaAtual].position.y);
             this.gameObject.transform.position = newPosition;
@@ -44,6 +53,7 @@ public class OndaMovement : MonoBehaviour
         }
         else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && ondaAtual > 0)
         {
+            AudioManager.Instance.sfxsrc.PlayOneShot(AudioManager.Instance.descend);
             ondaAtual -= 1;
             Vector2 newPosition = new Vector2(this.gameObject.transform.position.x, pontos[ondaAtual].position.y);
             this.gameObject.transform.position = newPosition;
@@ -61,18 +71,17 @@ public class OndaMovement : MonoBehaviour
     public void death()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector3.up, 10, ondaMask);
-
         if (timer < 5 || hit.collider == null || Mathf.Abs(hit.transform.position.y - transform.position.y)  < 2 ) return;
 
         GetComponentInChildren<Animator>().SetTrigger("dead");
         GetComponent<BoxCollider2D>().enabled = false;
         isDead = true;
         StartCoroutine(die());
-
+        AudioManager.Instance.sfxsrc.PlayOneShot(AudioManager.Instance.death);
 
         IEnumerator die()
         {
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(1f); // mudando o delay pro som da morte ser tocado corretamente
             SceneManager.LoadScene("Death");
         }
 
